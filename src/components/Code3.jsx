e04e28d387b67c1c154294fa88c80dcb90f5e7df
Bryon Hummel <bryonhummel@gmail.com>
2023-12-19 10:45:36 -0500

    initial layout done

--- src/components/Code3.jsx
+++ src/components/Code3.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 
 // https://flowbite.com/docs/components/timeline/
 
+function BlueSquare() {
+    return ( <span className='text-blue-800'>■</span>  );
+}
+function BlackDiamond() {
+    return ( <span className='text-black text-lg'>◆</span>  );
+}
+function GreenCircle() {
+    return ( <span className='text-green-600'>●</span>  );
+}
+
 function RunButton(props){
 
     var symb = '--'
     if (props.scale === "green") {
-        symb = '●' // green circle
+        symb = GreenCircle()
     } else if (props.scale === "blue") {
-        symb = '◆' // blue square
+        symb = BlueSquare()
     } else if (props.scale === "black") {
-        symb = '■' // black diamond
+        symb = BlackDiamond()
     }
-
-    console.log(symb)
 
     return(
         <div className='py-1'>
@@ -24,9 +32,12 @@ function RunButton(props){
 
 function TimelineToolbar(props) {
     return(
-        <div className='border border-dashed m-2 rounded-md bg-white grid sm:grid-cols-2 md:grid-cols-3 '>
-            <div className='mx-10 my-7'>
-                <ul class="text-left">
+        <div className='p-7 border border-dashed rounded-md bg-white grid sm:grid-cols-2 md:grid-cols-3 gap-6'>
+            <div className='flex'>
+                <div className='w-8 flex items-center justify-center bg-blue-100 text-blue-800 mr-2 my-1 rounded-md'>
+                    <span className='rotate-90'>Tenderfoot</span>
+                </div>
+                <ul class="text-left flex-1 flex-col">
                     <li><RunButton scale="green" name="Kid Zone"/></li>
                     <li><RunButton scale="green" name="Littlefoot"/></li>
                     <li><RunButton scale="green" name="Tenderfoot"/></li>
@@ -34,28 +45,35 @@ function TimelineToolbar(props) {
                     <li><RunButton scale="green" name="Bearsfoot"/></li>
                 </ul>
             </div>
-            <div className='mx-10 my-7'>
-                <ul class="text-left">
+            <div className='flex'>
+                <div className='w-8 flex items-center justify-center bg-blue-100 text-blue-800 mr-2 my-1 rounded-md'>
+                    <span className='rotate-90'>North</span>
+                </div>
+                <ul class="text-left flex-1 flex-col">
                     <li><RunButton scale="black" name="Camelshump"/></li>
                     <li><RunButton scale="blue" name="Easyrider"/></li>
                     <li><RunButton scale="blue" name="North"/></li>
                     <li><RunButton scale="blue" name="Mic Mac"/></li>
                 </ul>
             </div>
-            <div className='mx-10 my-7'>
-                <ul class="text-left">
+            <div className='flex'>
+                <div className='w-8 flex items-center justify-center bg-blue-100 text-blue-800 mr-2 my-1 rounded-md'>
+                    <span className='rotate-90'>Applebowl</span>
+                </div>
+                <ul class="text-left flex-1 flex-col">
+
                     <li><RunButton scale="blue" name="Chicane"/></li>
                     <li><RunButton scale="blue" name="Roughshod"/></li>
                     <li><RunButton scale="blue" name="Applebowl"/></li>
                     <li><RunButton scale="black" name="Racers Alley"/></li>
                 </ul>
             </div>
-            <div className='mx-10 mb-7'>
+            <div className=''>
                 <ul class="text-left">
                     <li><RunButton scale="na" name="Chalet"/></li>
                 </ul>
             </div>
-            <div className='mx-10 mb-7'>
+            <div className=''>
                 <ul class="text-left">
                     <li><RunButton scale="na" name="Other"/></li>
                 </ul>
@@ -66,11 +84,11 @@ function TimelineToolbar(props) {
 
 function Timeline(props) {
     return(
-    <div  className='border border-dashed m-2 rounded-md bg-white'>
+    <div  className='border border-dashed mt-4 rounded-md bg-white'>
     <ol class="m-10 relative border-s border-gray-200 dark:border-gray-700 text-left">                  
     <li class="mb-7 ms-6">            
         <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <span className='text-black'>■</span>
+            <BlueSquare/>
         </span>
         <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">North</h3>
         <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">21:13</time>
@@ -108,57 +126,12 @@ function Timeline(props) {
     )
 }
 
-function Timeline2(props) {
-    return(
-    <div className='border border-dashed m-2 rounded-md bg-white'>
-    <ol class="m-10 relative border-s border-gray-200 dark:border-gray-700 text-left">                  
-    <li class="mb-7 ms-6">            
-        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-            </svg>
-        </span>
-        <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">North</h3>
-        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">21:13</time>
-        <span class="bg-blue-100 text-blue-800 text-sm font-medium  px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 me-3">Bryon</span>
-        <span class="bg-blue-100 text-blue-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 me-3">Jenna</span>
-        <span class="me-3">&#171;</span>
-        <span class="bg-white border border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Mike</span>
-        <span class="bg-white border border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Bill</span>
-    </li>
-    <li class="mb-7 ms-6">            
-        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-            </svg>
-        </span>
-        <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">On Scene</h3>
-        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">21:15</time>
-        
-    </li>
-
-    <li class="mb-7 ms-6">            
-        <span class="absolute flex items-center justify-center w-6 h-6 bg-white border rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-            </svg>
-        </span>
-        <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Scene Clear</h3>
-        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">--:--</time>
-        
-    </li>
-    </ol>
-    </div>
-    )
-}
-
 
 function Code3() {
     return ( 
         <div>
             <TimelineToolbar/>
             <Timeline/>
-            <Timeline2/>
         </div>
      );
 }
