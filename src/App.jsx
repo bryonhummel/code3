7243d14d6e9ba0d55fc8dc746eee3fae42bec235
Bryon Hummel <bryonhummel@gmail.com>
2023-12-19 14:52:32 -0500

    basic multi-code support

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Code3 from './components/Code3'
 
+function NewCode(props) {
+  return(
+    <div><button onClick={props.onClick}>New Code 3</button></div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [codes, setCodes] = useState([])
+
+  function handleNewCode() {
+    var updatedCodes = [ new Date(), ...codes]
+    setCodes(updatedCodes)
+  }
+
+  function handleDeleteCode(codeKey) {
+    console.log(`handleDelete ${codeKey}`)
+    var filteredCodes = codes.filter(function(c) { return c !== codeKey })
+    setCodes(filteredCodes)
+  }
 
   return (
     <div className='max-w-3xl m-auto'>
-      <Code3/>
+      <NewCode onClick={handleNewCode}/>
+      {
+        codes.map((code) => (
+          <Code3 key={code} handleDelete={(e) => handleDeleteCode(code)} />
+        ))
+      }
     </div>
   )
 }
--- src/components/Code3.jsx
+++ src/components/Code3.jsx
@@ -53,7 +53,7 @@ function RunButton(props){
 function TimelineToolbar(props) {
 
     return(
-        <div className='p-7 border border-dashed rounded-md bg-white grid sm:grid-cols-2 md:grid-cols-3 gap-6'>
+        <div className='p-7 mt-7 border border-dashed rounded-md bg-white grid sm:grid-cols-2 md:grid-cols-3 gap-6'>
             <div className='flex'>
                 <div className='w-8 flex items-center justify-center bg-blue-100 text-blue-800 mr-2 my-1 rounded-md'>
                     <span className='rotate-90'>Tenderfoot</span>
@@ -104,42 +104,46 @@ function TimelineToolbar(props) {
 }
 
 function Timeline(props) {
+
+    const teamList = ['Mike', 'Bryon', 'Brigitte', 'Scott', 'JP', 'Chris', 'Kim', 'Bill', 'Jenna']
+    console.log(`Timeline.props.firstOn: ${props.firstOn}`)
+    var filteredTeamList = teamList.filter(function(e) { return !props.firstOn.includes(e) })
+    console.log(`filteredTeamList: ${filteredTeamList}`)
+
     return(
     <div  className='border border-dashed mt-4 rounded-md bg-white'>
     <ol className="m-10 relative border-s border-gray-200 dark:border-gray-700 text-left">                  
-    <li className="mb-7 ms-6">            
+    <li className="mb-7 ms-7">            
         <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
             <RunSymbol difficulty={props.runInfo.difficulty}/>
         </span>
         <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{props.runInfo.name}</h3>
         <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{props.runInfo.timestamp}</time>
         <div className="mb-4">
-        <div className="inline-flex bg-blue-100 text-blue-800 text-sm my-0.5 font-medium  px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 me-3">Bryon</div>
-        <div className="inline-flex bg-blue-100 text-blue-800 text-sm my-0.5 font-medium px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 me-3">Jenna</div>
-        <div className="inline-flex text-gray-300 text-sm my-0.5 font-medium px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 me-3">⇄</div>
+            {props.firstOn.map((name) => (
+            <div key={name} onClick={(e) => props.handleFirstOnToggle(name)} className="inline-flex bg-blue-100 text-blue-800 text-sm my-0.5 font-medium  px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 me-3">{name}</div>
+            ))}
         </div>
-        <div className="inline-flex bg-white border border-dashed my-0.5 border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Mike</div>
-        <div className="inline-flex bg-white border border-dashed my-0.5 border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Brigitte</div>
-        <div className="inline-flex bg-white border border-dashed my-0.5 border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Scott</div>
-        <div className="inline-flex bg-white border border-dashed my-0.5 border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">JP</div>
-        <div className="inline-flex bg-white border border-dashed my-0.5 border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Chris</div>
-        <div className="inline-flex bg-white border border-dashed my-0.5 border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Kim</div>
-        <div className="inline-flex bg-white border border-dashed my-0.5 border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Bill</div>
+        {
+            filteredTeamList.map((name) => (
+                <div key={name} onClick={(e) => props.handleFirstOnToggle(name)} className="inline-flex bg-white border border-dashed my-0.5 border-blue-100 text-blue-800 text-sm font-medium me-3 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{name}</div>
+            ))
+        }
     </li>
-    <li className="mb-7 ms-6">            
-        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+    <li className="ml-5 p-4 rounded-md cursor-pointer hover:bg-slate-100" onClick={props.handleOnSceneClick}>
+        <span className={`absolute flex items-center justify-center w-6 h-6 ${ props.onSceneTs ? 'bg-blue-100' : ''} rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900`}>
         <span className='text-blue-800'>⏲</span>
         </span>
         <h4 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">On Scene</h4>
-        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">21:15</time>
+        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{props.onSceneTs !== null ? props.onSceneTs : '--:--'}</time>
     </li>
 
-    <li className="mb-7 ms-6">            
-        <span className="absolute flex items-center justify-center w-6 h-6 bg-white border rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-        <span className='text-blue-800'>⏲</span>
+    <li className="ml-5 p-4 rounded-md cursor-pointer hover:bg-slate-100" onClick={props.handleSceneClearClick}>
+        <span className={`absolute flex items-center justify-center w-6 h-6 ${ props.sceneClearTs ? 'bg-blue-100' : ''} rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900`}>
+            <span className='text-blue-800'>⏲</span>
         </span>
         <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Scene Clear</h3>
-        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">--:--</time>
+        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{props.sceneClearTs !== null ? props.sceneClearTs : '--:--'}</time>
         
     </li>
     </ol>
@@ -147,21 +151,60 @@ function Timeline(props) {
     )
 }
 
+function getTimestamp() {
+    var ts = new Date()
+    ts = `${ts.getHours()}:${ts.getMinutes()}`
+    return ts
+}
 
-function Code3() {
+function Code3(props) {
     const [run, setRun] = useState(null);
+    const [onSceneTs, setOnSceneTs] = useState(null);
+    const [sceneClearTs, setSceneClearTs] = useState(null);
+    const [firstOn, setFirstOn] = useState([])
 
     function handleRunClick(name, difficulty) {
-        var ts = new Date()
-        ts = `${ts.getHours()}:${ts.getMinutes()}`
-        setRun({name: name, difficulty: difficulty, timestamp: ts})
+        setRun({name: name, difficulty: difficulty, timestamp: getTimestamp()})
     }
 
-    console.log(run)
+    function handleOnSceneClick(){
+        if (onSceneTs === null) {
+            setOnSceneTs(getTimestamp())
+        }
+    }
+
+    function handleSceneClearClick(){
+        if (sceneClearTs === null) {
+            setSceneClearTs(getTimestamp())
+        }
+    }
+
+    function handleFirstOnToggle(name) {
+        var filteredArray = []
+        if (firstOn.includes(name)) {
+            filteredArray = firstOn.filter(function(e) { return e !== name })
+        } else {
+            console.log(name)
+            filteredArray = [...firstOn]
+            filteredArray.push(name)
+            
+        }
+        console.log(filteredArray)
+        console.log(`setFirstOn ${firstOn} ${name} ${filteredArray}`)
+        setFirstOn(filteredArray)
+    }
+
     return ( 
         <div>
             { run == null && <TimelineToolbar handleRunClick={handleRunClick}/> }
-            { run !== null && <Timeline runInfo={run} /> }
+            { run !== null && <Timeline runInfo={run} 
+                onSceneTs={onSceneTs}
+                sceneClearTs={sceneClearTs}
+                firstOn={firstOn}
+                handleOnSceneClick={handleOnSceneClick}
+                handleSceneClearClick={handleSceneClearClick}
+                handleFirstOnToggle={handleFirstOnToggle} /> }
+            <button onClick={props.handleDelete}>delete</button>
         </div>
      );
 }
