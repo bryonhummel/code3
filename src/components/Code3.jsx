8d2658f405fda45ffc8c559b742798e07aa62a4f
Bryon Hummel <bryonhummel@gmail.com>
2023-12-20 12:03:09 -0500

    update svg clock

--- src/components/Code3.jsx
+++ src/components/Code3.jsx
@@ -111,7 +111,11 @@ function TimelineEntry(props) {
     return(
     <li className={`ml-5 mb-2 p-2 rounded-md ${timestamp === null ? 'cursor-pointer hover:bg-gray-100 border border-gray-200':'border border-white'}`} onClick={handleOnClick}>
         <span className={`absolute flex items-center justify-center w-6 h-6 ${ timestamp ? 'bg-gray-200' : 'bg-white'} border rounded-full -start-3 ring-8 ring-white`}>
-        <span className='text-gray-800'>⏲</span>
+        <span className='text-gray-800'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" data-slot="icon" className="w-4 h-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+        </span>
         </span>
         <h4 className="flex items-center mb-1 text-lg font-semibold text-gray-900">{text}</h4>
         <time className="block mb-2 text-sm font-normal leading-none text-gray-700">{timestamp !== null ? timestamp : '--:--'}</time>
