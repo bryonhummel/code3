a91a59a8f91b4b5501793456034f4a528b941532
Bryon Hummel <bryonhummel@gmail.com>
2023-12-22 15:42:43 -0500

    fix z-ordering of add button

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Code3 from './components/Code3'
 
 function NewCode(props) {
   return(
-    <div className='fixed bottom-4 right-4'>
+    <div className='fixed bottom-4 right-4 z-50'>
       <div onClick={props.onClick} className='rounded-full cursor-pointer bg-red-600 w-12 h-12 pb-0.5 flex items-center justify-center text-4xl md:hover:bg-red-500 active:bg-red-500 text-white '><span>+</span></div>
     </div>
   )
